a4eb5509727b8b6c8964dbd966dbc4a536214838
William Dignazio <slackwill@csh.rit.edu>
2012-01-04 22:38:27 -0500

    init commenting and code update

--- boot/init.asm
+++ boot/init.asm
@@ -1,10 +1,14 @@
-;; Kernel Grab Setup Program 
+;; Atlas Init 
 ;; Author: Will Dignazio
-;; Date: 10/23/2011
+;; Date: 01/05/2012 
 ;; Description: 
-;;	Kernel Grab program for the operating system that reads the rest 
-;; of the kernel to the physical memory. Also sets up a temporary GDT for 
-;; calling the kernel's start stublet. 
+;; 	Initializer of Atlas boot loader, it sets up the environment for 
+;; whatever code is to be run afterword. This environment includes
+;; a GDT, with kernel privelage for the entire 4gb of RAM of a IA32 
+;; system. It also enables the A20 line, which by most standards is
+;; not set at boot. After the A20 address line, and the GDT have been
+;; established, protected mode is set to give maximum power to the 
+;; code following this initializer. 
 [BITS 16]
 [extern start]
 [global init]
@@ -19,45 +23,39 @@ jmp init
 
 init:
   cli
-  xor ax, ax			; Set up a flat structure 
+  xor ax, ax				; Set up a flat structure 
   mov ds, ax
   mov es, ax
-  mov ax, 0x9000		; Stack at 0x9000
+  mov ax, 0x9000			; Stack at 0x9000
   mov ss, ax
-  mov sp, 0xFFFF		; Move the pointer way off.  
+  mov sp, 0xFFFF			; Move the pointer way off.  
   sti
   
   mov [bootdrv], dl
  .systemcheck:
-  call setvideo  		; Setting the video mode clears the screen. 
-  mov si, vidmode
-  call printxt
+  call setvideo  			; Setting the video mode clears the screen. 
   
-  mov si, grabinit
-  call printxt			; We've nearing the end of the boot process...
+  mov si, readinit
+  call printxt			
   
   mov si, isA20
   call printxt
-  call check_a20		; Check a20 status
-  call printAStatus		; Print The status
+  call check_a20			; Check a20 status
+  call printAStatus			; Print The status
 
   mov si, resetting
   call printxt
-  call devres			; Reset Disk Drive
+  call devres				; Reset Disk Drive
   
   mov si, readingkrn
   call printxt		
-  call kRead			; Read Kernel From Disk
+  call iLoad				; Read Kernel From Disk
   mov si, done
-  call printxt
-
-  ; Kernel Initialization 
-  mov si, kinit
   call printxt
 
   mov si, gdtinit 
   call printxt 
-  call gdt_install		; Install the gdt descriptors
+  call gdt_install			; Install the gdt descriptors
 
   mov si, pmodeinit
   call printxt
@@ -71,13 +69,14 @@ init:
 .End: 
 [BITS 32]					; We're now in 32 bit mode. 
   mov ax, DATA_DESCRIPTOR	; The process is done, we're ready to start 
-  mov ds, ax				; the kernel. 
+  mov ds, ax				; the user code. 
   mov ss, ax
   mov es, ax
   mov esp, 0x90000			; But first, we have to set the registers. 
-  call start				; Call the start stublet of the kernel 
+  call start				; Call the start Atlas shrugs to.  
 
-[BITS 16]
+[BITS 16] 					; We're back in 16 bits here for the functions of the loader. 
+
 ;; Print A20 Status
 ;;	- prints the status of the a20 line to the screen 
 printAStatus: 
@@ -107,13 +106,13 @@ setvideo:
   int 10h
   ret
 
-;; Read Kernel
-;;	- Loads the kernel to the virtual memory
+;; Init Load 
+;;	- Loads the code data to the physical memory
 ;;	- Drops it in ES:BX
 ;;	- ES:BX (start point) will be 0x500
 ;;	- Absolute address 50000
-kRead:
-  mov ax, 0x5000 	; Load kernel to 5000:bx
+iLoad:
+  mov ax, 0x5000 	; Load data to 5000:bx
   mov es, ax		; Set extra segment
   mov bx, 0x0   	; int 13h takes ES:BX
   mov ah, 2			; Read from drive 
@@ -125,7 +124,7 @@ kRead:
   ret
 
 ;; Drive Reset
-;;	- Resets the drive currently in use. 
+;;	- Resets the bootdrv. 
 devres:
   push ds			; Save Data Segment location
   mov ax, 0			; Function zero
@@ -134,23 +133,21 @@ devres:
   pop ds			; Recover data segment address
   ret
 
-yes db 		0x02, 'YES', 0x0D, 0x0A, 0x03
-no 	db		0x02, 'NO', 0x0D, 0x0A, 0x03
-isA20 db	0x02, '::A20 Enabled: ', 0x03
-vidmode	db	0x02, '::Setting Video Mode', 0x0D, 0x0A, 0x03
-readingkrn db	0x02, '::Reading Kernel From Disk', 0x0D, 0x0A, 0x03
-resetting db	0x02, '::Resetting Disk Drive', 0x0D, 0x0A, 0x03
-status db 	0x02, 'Status: ', 0x03
-errcode db	0x02, 'Error Code: ', 0x03
-done	db	0x02, '::Done.', 0x0A, 0x0D, 0x03
-notxt	db	0x02, 'ERROR, NOT A STRING', 0x0D, 0x0A, 0x03
-grabinit db	0x02, 'Kernel Grab Initiated:', 0x0D, 0x0A, 0x03
-kinit	db	0x02, 'Kernel Initializing:', 0x0D, 0x0A, 0x03
-gdtinit	db	0x02, '::Setting Up GDT', 0x0D, 0x0A, 0x03
-pmodeinit db 0x02, '::Entering Protected Mode', 0x0D, 0x0A, 0x03
-enablingA20 db 0x02, '::Enabling A20', 0x0D, 0x0A, 0x03
-bootdrv	db	0
+yes 		db 	0x02, 'YES', 0x0D, 0x0A, 0x03
+no 			db	0x02, 'NO', 0x0D, 0x0A, 0x03
+isA20 		db	0x02, '::A20 Enabled: ', 0x03
+readingkrn 	db	0x02, '::Reading Kernel From Disk', 0x0D, 0x0A, 0x03
+resetting 	db	0x02, '::Resetting Disk Drive', 0x0D, 0x0A, 0x03
+status 		db 	0x02, 'Status: ', 0x03
+errcode 	db	0x02, 'Error Code: ', 0x03
+done		db	0x02, '::Done.', 0x0A, 0x0D, 0x03
+notxt		db	0x02, 'ERROR, NOT A STRING', 0x0D, 0x0A, 0x03
+readinit 	db	0x02, 'Code Read Initiated:', 0x0D, 0x0A, 0x03
+gdtinit		db	0x02, '::Setting Up GDT', 0x0D, 0x0A, 0x03
+pmodeinit 	db 	0x02, '::Entering Protected Mode', 0x0D, 0x0A, 0x03
+enablingA20 db 	0x02, '::Enabling A20', 0x0D, 0x0A, 0x03
+bootdrv		db	0
 
 times 1022-($-$$) db 0  
-dw 'WD'
+dw 'WD'			; Signature to know where we are in a dump. 
 EXIT:
